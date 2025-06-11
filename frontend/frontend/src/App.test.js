5b3bee5ab0550b9351d6fceba0c768c60e09927b
Cesar <106403843+selazr@users.noreply.github.com>
2025-06-11 17:54:17 +0200

    feat: make sidebar collapsible

--- frontend/frontend/src/App.test.js
+++ frontend/frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login form', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const button = screen.getByText(/Ingresar/i);
+  expect(button).toBeInTheDocument();
 });
